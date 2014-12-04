d62fe5151c52572514c01b8b8b352780357f1c60
dreamsunday@163.com <dreamsunday@163.com>
2014-12-04 08:53:17 +0800

    version1.1.1

--- src/main/webapp/app/common/base/BaseController.js
+++ src/main/webapp/app/common/base/BaseController.js
@@ -7,7 +7,6 @@ Ext.define('somnus.common.base.BaseController', {
 				var view = this.getView(this.views[0]);
 				this.viewInstance = view.create();
 				this.viewInstance.close = function () {
-					console.log(view.prototype);
 					view.prototype.close.apply(this, arguments);
 					me.viewInstance = null;
 				};
