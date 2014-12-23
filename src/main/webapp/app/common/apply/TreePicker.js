941d4b250d4437430d836787e363d90eda3c41c4
dreamsunday@163.com <dreamsunday@163.com>
2014-12-23 16:17:52 +0800

    version1.1.1

---
+++ src/main/webapp/app/common/apply/TreePicker.js
@@ -0,0 +1,251 @@
+/**
+ * A Picker field that contains a tree panel on its popup, enabling selection of tree nodes.
+ */
+Ext.define('somnus.common.apply.TreePicker', {
+    extend: 'Ext.form.field.Picker',
+    xtype: 'treepicker',
+    
+    requires: ['Ext.tree.Panel'],
+
+    triggerCls: Ext.baseCSSPrefix + 'form-arrow-trigger',
+    
+    mixins: {
+        bindable: 'Ext.util.Bindable'    
+    },
+    config: {
+        store: null,
+
+        displayField: 'name',
+
+        columns: null,
+
+        selectOnTab: true,
+
+        maxPickerHeight: 300,
+
+        minPickerHeight: 100
+    },
+   
+    editable: false,
+
+    initComponent: function() {
+        var me = this;
+        
+        me.callParent(arguments);
+        
+        me.bindStore(me.store || 'ext-empty-store', true);
+
+        me.addEvents(
+            'select'
+        );
+
+        me.mon(me.store, {
+            scope: me,
+            load: me.onLoad,
+            update: me.onUpdate
+        });
+    },
+
+    /**
+     * Creates and returns the tree panel to be used as this field's picker.
+     */
+    createPicker: function() {
+        var me = this,
+            picker = new Ext.tree.Panel({
+                shrinkWrapDock: 2,
+                store: me.store,
+                floating: true,
+                displayField: me.displayField,
+                columns: me.columns,
+                minHeight: me.minPickerHeight,
+                maxHeight: me.maxPickerHeight,
+                manageHeight: false,
+                shadow: false,
+                listeners: {
+                    scope: me,
+                    itemclick: me.onItemClick
+                },
+                viewConfig: {
+                    listeners: {
+                        scope: me,
+                        render: me.onViewRender
+                    }
+                }
+            }),
+            view = picker.getView();
+
+        if (Ext.isIE9 && Ext.isStrict) {
+            view.on({
+                scope: me,
+                highlightitem: me.repaintPickerView,
+                unhighlightitem: me.repaintPickerView,
+                afteritemexpand: me.repaintPickerView,
+                afteritemcollapse: me.repaintPickerView
+            });
+        }
+        return picker;
+    },
+    
+    onViewRender: function(view){
+        view.getEl().on('keypress', this.onPickerKeypress, this);
+    },
+
+    /**
+     * repaints the tree view
+     */
+    repaintPickerView: function() {
+        var style = this.picker.getView().getEl().dom.style;
+
+        style.display = style.display;
+    },
+
+    /**
+     * Aligns the picker to the input element
+     */
+    alignPicker: function() {
+        var me = this,
+            picker;
+
+        if (me.isExpanded) {
+            picker = me.getPicker();
+            if (me.matchFieldWidth) {
+                picker.setWidth(me.bodyEl.getWidth());
+            }
+            if (picker.isFloating()) {
+                me.doAlign();
+            }
+        }
+    },
+
+    /**
+     * Handles a click even on a tree node
+     * @private
+     * @param {Ext.tree.View} view
+     * @param {Ext.data.Model} record
+     * @param {HTMLElement} node
+     * @param {Number} rowIndex
+     * @param {Ext.EventObject} e
+     */
+    onItemClick: function(view, record, node, rowIndex, e) {
+        this.selectItem(record);
+    },
+
+    /**
+     * Handles a keypress event on the picker element
+     * @private
+     * @param {Ext.EventObject} e
+     * @param {HTMLElement} el
+     */
+    onPickerKeypress: function(e, el) {
+        var key = e.getKey();
+
+        if(key === e.ENTER || (key === e.TAB && this.selectOnTab)) {
+            this.selectItem(this.picker.getSelectionModel().getSelection()[0]);
+        }
+    },
+
+    /**
+     * Changes the selection to a given record and closes the picker
+     * @private
+     * @param {Ext.data.Model} record
+     */
+    selectItem: function(record) {
+        var me = this;
+        me.setValue(record.getId());
+        me.picker.hide();
+        me.inputEl.focus();
+        me.fireEvent('select', me, record)
+
+    },
+
+    /**
+     * Runs when the picker is expanded.  Selects the appropriate tree node based on the value of the input element,
+     * and focuses the picker so that keyboard navigation will work.
+     * @private
+     */
+    onExpand: function() {
+        var me = this,
+            picker = me.picker,
+            store = picker.store,
+            value = me.value,
+            node;
+
+        
+        if (value) {
+            node = store.getNodeById(value);
+        }
+        
+        if (!node) {
+            node = store.getRootNode();
+        }
+        
+        picker.selectPath(node.getPath());
+
+        Ext.defer(function() {
+            picker.getView().focus();
+        }, 1);
+    },
+
+    /**
+     * Sets the specified value into the field
+     * @param {Mixed} value
+     * @return {Ext.ux.TreePicker} this
+     */
+    setValue: function(value) {
+        var me = this,
+            record;
+
+        me.value = value;
+
+        if (me.store.loading) {
+            return me;
+        }
+            
+        /*record = value ? me.store.getNodeById(value) : me.store.getRootNode();*/
+        console.log(me);
+        console.log(me.store);
+        if (value === undefined) {
+            record = me.store.getRootNode();
+            me.value = record.getId();
+        } else {
+            record = me.store.getNodeById(value);
+        }
+
+        me.setRawValue(record ? record.get(me.displayField) : '');
+
+        return me;
+    },
+    
+    getSubmitValue: function(){
+        return this.value;    
+    },
+
+    /**
+     * Returns the current data value of the field (the idProperty of the record)
+     * @return {Number}
+     */
+    getValue: function() {
+        return this.value;
+    },
+
+    /**
+     * Handles the store's load event.
+     * @private
+     */
+    onLoad: function() {
+        var value = this.value;
+
+        if (value) {
+            this.setValue(value);
+        }
+    },
+    
+    onUpdate: function(store, rec, type, modifiedFieldNames){
+        var display = this.displayField;
+        
+        if (type === 'edit' && modifiedFieldNames && Ext.Array.contains(modifiedFieldNames, display) && this.value === rec.getId()) {
+            this.setRawValue(rec.get(display));
+        }
+    }
+
+});
