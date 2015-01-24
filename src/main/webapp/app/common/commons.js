82fd3d3be6aa8866c810fa11148c2f0ee1e0cb83
love-somnus <928200207@qq.com>
2015-01-24 17:46:48 +0800

    version1.1.1

--- src/main/webapp/app/common/commons.js
+++ src/main/webapp/app/common/commons.js
@@ -84,23 +84,17 @@ Ext.apply(Ext.form.field.VTypes, {
             return false;
         }
         if (field.startDateField && (!this.dateRangeMax || (date.getTime() != this.dateRangeMax.getTime()))) {
-            console.log(field.ownerCt.down('#' + field.startDateField));
         	var start = field.ownerCt.down('#' + field.startDateField);
             start.setMaxValue(date);
             start.validate();
             this.dateRangeMax = date;
         }
         else if (field.endDateField && (!this.dateRangeMin || (date.getTime() != this.dateRangeMin.getTime()))) {
-        	console.log(field.ownerCt.down('#' + field.endDateField));
         	var end = field.ownerCt.down('#' + field.endDateField);
             end.setMinValue(date);
             end.validate();
             this.dateRangeMin = date;
         }
-        /*
-         * Always return true since we're only using this vtype to set the
-         * min/max allowed values (these are tested for after the vtype test)
-         */
         return true;
     },
     daterangeText: 'Start date must be less than end date',
